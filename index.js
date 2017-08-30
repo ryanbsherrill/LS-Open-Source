e46e7eafcdca17737e528769291563f5e864be28
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-08-29 21:09:42 -0400

    added s

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(`<h1>HELLO WORLD!</h1>`);
+  res.send(`<h1>HELLO WORLDS!</h1>`);
 });
 
 app.listen(PORT, () => {
